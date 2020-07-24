b3b2a0e102790e485bc7bc7b6453995de17c6e8a
Sara Correia <sara.dsc@hotmail.com>
2020-07-24 15:43:12 +0100

    finished orders controller

--- backend/src/controllers/OrderController.js
+++ backend/src/controllers/OrderController.js
@@ -5,6 +5,8 @@ module.exports = {
 
     async index(request, response) {
         const orders = await connection('orders').select('*');
+
+        return response.json(orders);
     },
 
     async create(request, response) {
@@ -19,5 +21,20 @@ module.exports = {
         });
 
         return response.json({ id });
+    },
+
+    async delete(request, response) {
+        const { id } = request.params;
+        const company_id = request.headers.authorization;
+
+        const order = await connection('orders').where('id', id).select('company_id').first();
+
+        if (order.company_id !== company_id) {
+            return response.status(401).json({ error: 'Operation denied' }); 
+        }
+
+        await connection('orders').where('id', id).delete();
+
+        return response.status(204).send();
     }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,10 +6,10 @@ const orderController = require('./controllers/OrderController');
 const routes = express.Router();
 
 routes.get('/companies', companyController.index);
-
 routes.post('/companies', companyController.create);
 
-routes.get('/orders', orderController.create);
+routes.get('/orders', orderController.index);
 routes.post('/orders', orderController.create);
+routes.delete('/orders/:id', orderController.delete);
 
 module.exports = routes;
