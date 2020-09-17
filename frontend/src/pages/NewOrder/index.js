2f0c829d5fd6168f349343f040eb337b51950e4e
Sara Correia <sara.dsc@hotmail.com>
2020-09-17 17:34:10 +0100

    finished web app

--- frontend/src/pages/NewOrder/index.js
+++ frontend/src/pages/NewOrder/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -8,6 +10,38 @@ import blueberriesImg from '../../assets/blueberries.png';
 import miniLogoImg from '../../assets/minilogo.png';
 
 export default function NewOrder() {
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+
+    const history = useHistory();
+
+    const companyId = localStorage.getItem('companyId');
+
+    async function handleNewOrder(e) {
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value
+        };
+
+        try {
+
+            await api.post('orders', data, {
+                headers: {
+                    Authorization: companyId
+                }
+            });
+
+            history.push('/profile');
+        } catch (err) {
+            alert('Error registering order. Try again.');
+        }
+    }
+
     return (
         <div className="new-order-container">
             <img src={miniLogoImg} alt="Blueberry" style={{ position: "absolute", top: "30px", left: "30px" }} />
@@ -20,11 +54,22 @@ export default function NewOrder() {
                     <img src={blueberriesImg} alt="Blueberries" />
                 </section>
 
-                <form>
-                    <input placeholder="Title Description" />
-                    <textarea placeholder="Description" />
-
-                    <input placeholder="Value" />
+                <form onSubmit={handleNewOrder}>
+                    <input
+                        placeholder="Title Description"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <textarea
+                        placeholder="Description"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
+                    <input
+                        placeholder="Value"
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
 
                     <button className="button" type="submit">Register</button>
                     <Link className="back-link" to="/profile"><FiArrowLeft size={16} color="#3c5488" />Homepage</Link>
